643cd80b7c414c1e194dc8e80f80d3bc609313bc
Leandro Canabarro <leandrocanabarro@gmail.com>
2020-07-12 21:53:10 -0300

    using github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,81 +1,102 @@
 import React from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
-import { Container, Title, Form, Repositories } from './styles';
+import { Container, Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = React.useState('');
+  const [inputError, setInputError] = React.useState('');
+  const [repositories, setRepositories] = React.useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+
+    return [];
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const reponse = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = reponse.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <Container>
-      <img
-        src={logo}
-        alt="GitHub Explorer"
-      />
+      <img src={logo} alt="GitHub Explorer" />
 
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           name="repository"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
           placeholder="Digite o nome do repositório"
         />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <li>
-          <Link to="/repositories">
-            <img
-              src="https://avatars2.githubusercontent.com/u/662405?s=400&u=c16321ccf0ffdc5e0ff36a9e4c61ec966166a2d1&v=4"
-              alt="Leandro Canabarro"
-            />
-            <div>
-              <strong>star-wars</strong>
-              <p>Projeto desenvolvido utilizando ReactJS + SWAPI.</p>
-            </div>
-            <FiChevronRight />
-          </Link>
-        </li>
-        <li>
-          <Link to="/repositories">
-            <img
-              src="https://avatars2.githubusercontent.com/u/662405?s=400&u=c16321ccf0ffdc5e0ff36a9e4c61ec966166a2d1&v=4"
-              alt="Leandro Canabarro"
-            />
-            <div>
-              <strong>star-wars</strong>
-              <p>Projeto desenvolvido utilizando ReactJS + SWAPI.</p>
-            </div>
-            <FiChevronRight />
-          </Link>
-        </li>
-        <li>
-          <Link to="/repositories">
-            <img
-              src="https://avatars2.githubusercontent.com/u/662405?s=400&u=c16321ccf0ffdc5e0ff36a9e4c61ec966166a2d1&v=4"
-              alt="Leandro Canabarro"
-            />
-            <div>
-              <strong>star-wars</strong>
-              <p>Projeto desenvolvido utilizando ReactJS + SWAPI.</p>
-            </div>
-            <FiChevronRight />
-          </Link>
-        </li>
-        <li>
-          <Link to="/repositories">
-            <img
-              src="https://avatars2.githubusercontent.com/u/662405?s=400&u=c16321ccf0ffdc5e0ff36a9e4c61ec966166a2d1&v=4"
-              alt="Leandro Canabarro"
-            />
-            <div>
-              <strong>star-wars</strong>
-              <p>Projeto desenvolvido utilizando ReactJS + SWAPI.</p>
-            </div>
-            <FiChevronRight />
-          </Link>
-        </li>
+        {repositories.map(repository => (
+          <li>
+            <Link
+              key={repository.full_name}
+              to={`/repositories/${repository.full_name}`}
+            >
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+          </li>
+        ))}
       </Repositories>
     </Container>
   )
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Container = styled.section`
   width: 100%;
@@ -9,13 +13,13 @@ export const Container = styled.section`
 
 export const Title = styled.h1`
   font-size: 48px;
-  color: #3A3A3A;
+  color: #3a3a3a;
   max-width: 450px;
   line-height: 56px;
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   width: 100%;
@@ -27,10 +31,18 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
-    color: #3A3A3A;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
-      color: #A8A8B3
+      color: #a8a8b3;
     }
   }
 
@@ -45,9 +57,15 @@ export const Form = styled.form`
     transition: background-color 0.2s;
 
     &:hover {
-    background: ${shade(0.2, '#04d361')};
+      background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.ul`
@@ -82,23 +100,24 @@ export const Repositories = styled.ul`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
-        color: #3D3D4D;
+        color: #3d3d4d;
       }
 
       p {
         margin-top: 4px;
         font-size: 18px;
-        color: #A8A8B3;
+        color: #a8a8b3;
       }
     }
 
     svg {
       margin-left: auto;
-      color: #CBCBD6;
+      color: #cbcbd6;
       font-size: 32px;
     }
   }
